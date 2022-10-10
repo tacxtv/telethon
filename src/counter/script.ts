ee37451a177894631d2c318e7bc3670bdbb9d1b0
Tacx <12997062+tacxticx88@users.noreply.github.com>
2022-10-10 18:21:16 +0200

    counter

---
+++ src/counter/script.ts
@@ -0,0 +1,18 @@
+'use strict'
+
+import * as dayjs from 'dayjs'
+import 'dayjs/locale/fr'
+
+dayjs.locale('fr')
+
+document.addEventListener('DOMContentLoaded', () => {
+    const now = dayjs()
+    const target = dayjs('2022-11-11 18:00:00')
+    const diff = target.diff(now, 'days')
+    if (diff <= 0) {
+        const diff2 = target.diff(now, 'hours')
+        document.getElementById('text').innerText = 'H-' +  diff2.toString()
+    } else {
+        document.getElementById('text').innerText = 'J-' +  diff.toString()
+    }
+})
